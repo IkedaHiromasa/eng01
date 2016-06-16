881d76fd15fd5ab30a8ae7378d43c8caaedce1f8
hikeda <ikemegae19890921@gmail.com>
2016-06-17 04:17:15 +0900

    [WIP]池田_Level3

---
+++ June/ikeda/Level4/Level4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Level4
+{
+    public void execute(String[] student, int group) throws Exception
+    {
+        System.out.println("まだ答えがないよ");
+    }
+}
