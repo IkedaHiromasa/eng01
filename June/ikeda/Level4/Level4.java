e67b2f489aa19d8eb0202ac1e0aae0b14dcc6c54
hikeda <ikemegae19890921@gmail.com>
2016-06-17 04:25:25 +0900

    [WIP]池田_Level4

--- June/ikeda/Level4/Level4.java
+++ June/ikeda/Level4/Level4.java
@@ -1,11 +1,44 @@
 import java.util.*;
+import java.util.Map.Entry;
 import java.lang.*;
 import java.io.*;
 
-public class Level4
-{
-    public void execute(String[] student, int group) throws Exception
-    {
-        System.out.println("まだ答えがないよ");
+public class Level4 {
+    public void execute(String[] student, int group) throws Exception {
+        System.out.println("考え中,,,,,,");
+        if (student == null || student.length == 0) { throw new Exception("生徒がいません"); }
+
+        Map<String, List<String>> hogehoge = new LinkedHashMap<>(group);
+
+        System.out.println("商"+ (student.length / group));
+        System.out.println("あまり"+ (student.length % group));
+        // 最小人数
+        int minCount = student.length / group;
+        // 余り人数
+        int remainderCount = student.length % group;
+        // 最大人数
+        int maxCount =student.length / group == 0 ? student.length / group : (student.length / group + 1);
+
+        List<String> hoge =new ArrayList<>();
+        int count = 0;
+        int teamCount = 1;
+        for (String studentName : student) {
+            System.out.println("配列中身"+studentName);
+            hoge.add(studentName);
+            if (hoge.size() == maxCount) {
+                hogehoge.put("グループ" + teamCount, hoge);
+                teamCount++;
+                hoge.clear();
+            }
+            count++;
+        }
+
+
+        for (Entry<String, List<String>> e :hogehoge.entrySet()) {
+            System.out.println(e.getKey());
+            for (String name : e.getValue()) {
+                System.out.println(name);
+            }
+        }
     }
 }
